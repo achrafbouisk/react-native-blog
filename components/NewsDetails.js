fe9d024ac7c07e4661d821d64acc89a7ca3250d5
ESAAIDI Akram <87911826+AkramDev1@users.noreply.github.com>
2022-05-11 20:47:26 +0200

    'test_collab'

---
+++ components/NewsDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const NewsDetails = () => {
+  return (
+    <View>
+      <Text>News FK</Text>
+    </View>
+  );
+};
+
+export default NewsDetails;
